dfa438e041c6c7541bc1509dc35c9479beeb9271
julia <juliabiersuriano@gmail.com>
2022-09-13 16:26:06 -0400

    finished version

--- handler.js
+++ handler.js
@@ -5,35 +5,36 @@ const moment = require('moment')
 
 require('dotenv').config();
 
-
-const config = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-}
-/*
-const Twitter = new twit(config);
-
-
-Twitter.post('/statuses/update', { status: moment("12/09/2022", "DD-MM-YYYY").diff(moment(process.env.THE_DATE, "DD-MM-YYYY")) }, function(err, data, response) {
-  if (err) {
-    console.log(`Tweet was not posted. Errorcode: ${err}`)
-  } else {
-    console.log('Tweet posted.')
+exports.handler = (event, context, callback) => {
+  const config = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
   }
-})
-*/
 
-const currentDate = moment();
-const nextDate = moment(process.env.THE_DATE, "DD-MM-YYYY HH:mm");
+  const Twitter = new twit(config);
 
-const daysLeft = nextDate.diff(currentDate, 'days');
+  const currentDate = moment();
+  const nextDate = moment(process.env.THE_DATE, "DD-MM-YYYY HH:mm");
 
-if (daysLeft > 1) {
-  console.log(`te vejo em ${daysLeft} dias`)
-}
+  const daysLeft = nextDate.diff(currentDate, 'days');
 
-else if (daysLeft === 1) {
-  console.log('te vejo amanhã')
+  let message = '';
+
+  if (daysLeft > 1) {
+    message = `te vejo em ${daysLeft} dias`;
+  } else if (daysLeft === 1) {
+    message = 'te vejo amanhã';
+  }
+
+  if (message != '') {
+    Twitter.post('/statuses/update', { status: message }, function(err, data, response) {
+      if (err) {
+        console.log(`Tweet was not posted. Errorcode: ${err}`)
+      } else {
+        console.log('Tweet posted.')
+      }
+    });
+  }
 }
